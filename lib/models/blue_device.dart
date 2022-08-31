509af8c35597e32aec6750b09dfd67ac94d0ac21
Sachin Gowda <sachin08gowda@gmail.com>
2022-08-31 18:33:42 +0530

    added fromMap on BlueDevice

--- lib/models/blue_device.dart
+++ lib/models/blue_device.dart
@@ -1,3 +1,6 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
 import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 
 class BlueDevice {
@@ -25,4 +28,63 @@ class BlueDevice {
 
   int? type;
   bool? connected;
+
+  BlueDevice copyWith({
+    String? name,
+    String? address,
+    int? type,
+    bool? connected,
+  }) {
+    return BlueDevice(
+      name: name ?? this.name,
+      address: address ?? this.address,
+      type: type ?? this.type,
+      connected: connected ?? this.connected,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'name': name,
+      'address': address,
+      'type': type,
+      'connected': connected,
+    };
+  }
+
+  factory BlueDevice.fromMap(Map<String, dynamic> map) {
+    return BlueDevice(
+        name: map['name'] as String,
+        address: map['address'] as String,
+        type: map['type'] as int,
+        connected: map['connected'] as bool);
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory BlueDevice.fromJson(String source) =>
+      BlueDevice.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  @override
+  String toString() {
+    return 'BlueDevice(name: $name, address: $address, type: $type, connected: $connected)';
+  }
+
+  @override
+  bool operator ==(covariant BlueDevice other) {
+    if (identical(this, other)) return true;
+
+    return other.name == name &&
+        other.address == address &&
+        other.type == type &&
+        other.connected == connected;
+  }
+
+  @override
+  int get hashCode {
+    return name.hashCode ^
+        address.hashCode ^
+        type.hashCode ^
+        connected.hashCode;
+  }
 }
